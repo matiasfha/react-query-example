e7e73c95fe6d0335cb0f21f453ef36739401e77f
Matias Hernandez <matiasfh@gmail.com>
2021-04-27 06:56:21 -0400

    useQuery for query hooks

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,28 +1,9 @@
 import React from "react";
 import axios from "axios";
-import useFetchState from './useFetchState'
+import { useQuery } from 'react-query'
 
 export const fetchTodo = (todoId) => axios.get(`/api/todos/${todoId}`).then((res => res.data))
 
 export default function useTodo(todoId) {
-    const { state, startFetch, success, error } = useFetchState()
-    const refetch = React.useCallback(async () => {
-        startFetch()
-        try {
-            const data = await fetchTodo(todoId)
-            success(data)
-        } catch (e) {
-            console.log(e)
-            error(e)
-        }
-    }, [])
-
-    React.useEffect(() => {
-        refetch();
-    }, []);
-
-    return {
-        ...state,
-        refetch
-    };
+    return useQuery(['todo', todoId], () => fetchTodo(todoId))
 }
